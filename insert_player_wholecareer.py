4d879bb81cfe2c803fa94db2362a5b4f75495bb2
dennissuperstar <v.eridicaldeveloper@gmail.com>
2020-06-22 00:57:37 +0800

    player whole carrer updated!

--- insert_player_wholecareer.py
+++ insert_player_wholecareer.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import argparse
 import mysql.connector
+
 import certifi
 import urllib3
 
@@ -15,7 +16,7 @@ http = urllib3.PoolManager( cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  passwd="Password@302",
+  passwd="",
   database="soccer"
 )
 mycursor = mydb.cursor()
@@ -23,8 +24,8 @@ def switch_season(argument):
     switcher = {
       "2014-2015": 1,
       "2015-2016": 2,
+      "2016-2017_2" : 3,
       "2016-2017": 3,
-    
       "2017-2018": 4,
       "2018-2019": 5,
         "2014": 6,
@@ -37,117 +38,177 @@ def switch_season(argument):
     return switcher.get(argument, "null")
 def switch_league(argument):
     switcher = {
-      "esp-primera-division": 16,
-      "eng-premier-league": 6,
-      "bundesliga": 8,   #germany
-      "ita-serie-a" : 11,  #italy
-      "fra-ligue-1" : 7,   #france
-      "ned-eredivisie": 12,  #Netherland
-      "aut-bundesliga": 1,  #Austria
-        "por-primeira-liga": 14,  #portugal
-        "gre-superleague": 9,   #Greece
-        "tur-sueperlig": 19,   #Turkey
-        "nor-eliteserien": 13,  #Norway
-        "swe-allsvenskan": 17,  #Sweden
-        "sui-super-league": 18,   #Swiztland
-        "den-superliga": 5,     #Denmark
-        "ukr-premyer-liga": 20,     #Ukraine
-        "bul-a-grupa": 2,       #bulgaria
-        "cze-1-fotbalova-liga": 3,      #Chezch
-        "cro-1-hnl": 4 ,          #Croatia
-        "hun-nb-i": 10,     #Hungary
+        "esp-primera-division": 16 , #spain
         "srb-super-liga": 15    #Serbia
     }
     return switcher.get(argument, "null")
+def insert_player_wholecareer(season=None , league=None, pageNumber = None):
 
-def scrape_season_league_teamname(season=None , league=None):
-
-    if season:
-        URL = f"https://www.worldfootball.net/players/{league}-{season}"
-    else:
-        URL = f"https://www.worldfootball.net/players/eng-premier-league-2014-2015/"
-    page = requests.get(URL)
+    ################################### page url check start ###############################
+    print(f"-------------------------------{season}-{pageNumber}page start-----------------------------------------")
+    URL = f"https://www.worldfootball.net/players_list/{league}-{season}/nach-mannschaft/{pageNumber}/"
+    page = requests.get(URL , headers={"User-Agent":"Mozilla/5.0"})
     soup = BeautifulSoup(page.content, "html.parser")
-
     results = soup.find('table', class_="standard_tabelle")
     tr_results = results.find_all("tr")
-    team_info = []
+
+    i = 1
     for tr in tr_results:
-        td = tr.find("td")
-        img_src = td.find("img")['src']
-        teamname = td.find("img")['title']
-        ev_team = [img_src, teamname]
-        team_info.append(ev_team)
-        #print(f"img : {img_src} , title : {teamname}")
+        all_td = tr.find_all("td")
+        http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+        if(len(all_td)) :
+            
+            player_name = all_td[0].text
+            player_born = all_td[3].text
+            sql = f'SELECT * FROM playerlist WHERE player_name ="{player_name}" and birthday = "{player_born}"'
+            mycursor.execute(sql)
+            myresult = mycursor.fetchone()
+            if len(myresult) == 0:
+                print(f"Error find playerid of {player_name}")
+                return
+            player_id = myresult[0]
+            print(f"--------------{pageNumber}-{i}th player : id- {player_id} data handling start!-----------")
+            sql = f'SELECT * FROM player_career WHERE player_id ="{player_id}"'
+            mycursor.execute(sql)
+            myresult = mycursor.fetchall()
+            if len(myresult):
+                print(f"{player_id}th data is already added!")
+            else:
+                href_info = all_td[0].find("a")['href']
+                url = "https://www.worldfootball.net"+href_info+"/2/"
+                page = requests.get(url , headers={"User-Agent":"Mozilla/5.0"})
+                soup = BeautifulSoup(page.content, "html.parser")
+                ################################### page url check end ###############################
+                ################################### career check start ###############################
+                extra_results = soup.find('table', class_="standard_tabelle")
 
-    return team_info
+                extra_tr_results = extra_results.find_all("tr")
+               
+                count = 1
+                tr_index = 1
+                for tr in extra_tr_results:
+                    all_td = tr.find_all("td")
+                    http = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
+                    if(len(all_td)) :
+                        if (tr_index > 1) and len(all_td) < 2: # no carrer
+                            break
+                        else: 
+                            flag = all_td[0].find('img')['src']
+                            league_id = fn_Get_LeagueId(all_td[1].text, all_td[1].find('a')['href'])
+                            season_id = fn_Get_SeasonId(all_td[2].text)
+                            team_id = fn_Get_TeamId(all_td[3].text)
+                            
+                            sql = f"INSERT INTO player_career (player_id, flag, league_id, season_id, team_id, matches, goals, started,s_in, s_out, yellow, s_yellow, red ) VALUES ({player_id},'{flag}', {league_id} ,{ season_id}, {team_id}, \
+                                {  fn_filter_value(all_td[4].text)}, \
+                                { fn_filter_value(all_td[5].text)}, \
+                                { fn_filter_value(all_td[6].text)}, \
+                                { fn_filter_value(all_td[7].text)}, \
+                                { fn_filter_value(all_td[8].text)}, \
+                                { fn_filter_value(all_td[9].text)}, \
+                                { fn_filter_value(all_td[10].text)}, \
+                                { fn_filter_value(all_td[11].text)})"
+                            
+                            mycursor.execute(sql)
+                            mydb.commit()
+                            print(f"   added extra new row-{count}")
+                            count = count + 1
+                    tr_index = tr_index +1
+                        
+            
+            print(f"--------------{pageNumber}-{i}th player : id- {player_id} : name: {player_name}'s data handling End !-----------")
+            i = i+1
 
-def print_scrape_season_league_teamname(season=None , league=None):
-    if season == None:
-        print(f"Enter the season and league")
-    else:
-        team_info = scrape_season_league_teamname(season,league)
-        i=0
-        for  info in team_info:
-            i+=1
-            print(f"{season}'s {league} match {i}team src: {info[0]} -- name: {info[1]}\n")
-
-def insert_teamList(season=None, league=None):
-    if season == None:
-        print("Enter the season and league!")
-        return
-    teamNameList = scrape_season_league_teamname(season, league)
-
-    for team_info in teamNameList:
-        teamname = team_info[1]
-        team_src = team_info[0]
-
-        sql = f"SELECT * FROM team_list WHERE team_name ='{teamname}'"
-        mycursor.execute(sql)
-        myresult = mycursor.fetchall()
-        if len(myresult):
-            print(f"There is already in team list - {teamname}")
+def fn_filter_value(str):
+    
+    if '?' in str:
+        return 0
+    else: 
+        return int(str)
+                    
+def fn_Get_LeagueId(league_dname, league_extra_info):
+    realLeague = league_extra_info.split("/")[2]
+    if league_dname == "Bundesliga":
+        #realLeague = league_extra_info.split("/")[2]
+        if realLeague == "bundesliga":
+            return 8
+        if realLeague == "aut-bundesliga":
+            return 1
         else:
-            print(f"this is new - {teamname}")
-            sql = "INSERT INTO team_list (team_name, league_id , img_src) VALUES (%s, %s , %s)"
-            val = (teamname, switch_league(league), team_src)
-            mycursor.execute(sql, val)
-            mydb.commit()
-            print(mycursor.rowcount, "record inserted.")
-
-         #insert teamlist end
-
-        sql = f"SELECT team_id FROM team_list WHERE team_name = '{teamname}'"
+            sql = f'SELECT league_id FROM league where league_title = "{realLeague}"'  # if league dname's duplicate exists, then search league_title
+            mycursor.execute(sql)
+            myresult = mycursor.fetchone()
+            if myresult:
+                return myresult[0]
+            else:
+                sql = f'INSERT INTO league (league_dname , league_title ) VALUES ("{league_dname}, {realLeague}")'
+                mycursor.execute(sql)
+                mydb.commit()
+                print("   -------added new league-"+league_dname + ":"+ realLeague)
+                return mycursor.lastrowid
+    if league_dname == "Super League":
+       # realLeague = league_extra_info.split("/")[2]
+        if realLeague == "gre-super-league":
+            return 9
+        if realLeague == "sui-super-league":
+            return 18
+        else:
+            sql = f'SELECT league_id FROM league where league_title = "{realLeague}"'  # if league dname's duplicate exists, then search league_title
+            mycursor.execute(sql)
+            myresult = mycursor.fetchone()
+            if myresult:
+                return myresult[0]
+            else:
+                sql = f'INSERT INTO league (league_dname , league_title ) VALUES ("{league_dname}, {realLeague}")'
+                mycursor.execute(sql)
+                mydb.commit()
+                print("   -------added new league-"+league_dname + ":"+ realLeague)
+                return mycursor.lastrowid
+    
+    sql = f'SELECT league_id FROM league where league_dname = "{league_dname}"'
+    mycursor.execute(sql)
+    myresult = mycursor.fetchone()
+    if myresult:
+        return myresult[0]
+    else:
+        sql = f'INSERT INTO league (league_dname ) VALUES ("{league_dname}")'
         mycursor.execute(sql)
-        myresult = mycursor.fetchone()
-        team_id = myresult[0]
-
-        sql = "INSERT INTO season_league_team_info (season_id, league_id, team_id) VALUES (%s, %s , %s)"
-        val = (switch_season(season), switch_league(league) , team_id)
-        mycursor.execute(sql, val)
         mydb.commit()
-        print("insert season_league_team end!")
+        print("   -------added new league-"+league_dname + ":"+ realLeague)
+        return mycursor.lastrowid
 
+def fn_Get_SeasonId(season_title):
+    sql = f'SELECT season_id FROM season where season_title = "{season_title}"'
+    mycursor.execute(sql)
+    myresult = mycursor.fetchone()
+    if myresult:
+        return myresult[0]
+    else:
+        sql = f'INSERT INTO season (season_title ) VALUES ("{season_title}")'
+        mycursor.execute(sql)
+        mydb.commit()
+        print("   ----------added new season-" + season_title)
+        return mycursor.lastrowid
 
+def fn_Get_TeamId(team_name):
+    sql = f'SELECT team_id FROM team_list where team_name = "{team_name}"'
+    mycursor.execute(sql)
+    myresult = mycursor.fetchone()
+    if myresult:
+        return myresult[0]
+    else:
+        sql = f'INSERT INTO team_list (team_name ) VALUES ("{team_name}")'
+        
+        mycursor.execute(sql)
+        mydb.commit()
+        print("   ---------added new team-" + team_name)
+        return mycursor.lastrowid
 
-my_parser = argparse.ArgumentParser(
-    prog="jobs", description="Find Developer Jobs"
-)
-my_parser.add_argument(
-    "-league", metavar="league", type=str, help="The location of the job"
-)
-my_parser.add_argument(
-    "-season", metavar="season", type=str, help="What keyword to filter by"
-)
+def main():
+    for i in range(13, 14):
+        insert_player_wholecareer("2014-2015", "esp-primera-division" , i)
 
-args = my_parser.parse_args()
-season, league = args.season, args.league
-
-#print_scrape_season_league_teamname(season, league)
-seasonList = ["2014-2015","2015-2016","2016-2017","2017-2018","2018-2019"]
-#seasonList = ["2015","2016","2017","2018","2019"]
-for season in seasonList:
-    insert_teamList(season, "srb-super-liga")
+if '__main__' == __name__:
+    main()
 
 
 
